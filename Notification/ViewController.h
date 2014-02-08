e43e583b4bb3880d3c097b01b2818f8e4305b118
mingcute <bestdearlove@gmail.com>
2014-02-08 12:39:20 +0800

    Initial Commit

---
+++ Notification/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Notification
+//
+//  Created by 李健銘 on 2014/2/8.
+//  Copyright (c) 2014年 TakoBear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Notification/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Notification
+//
+//  Created by 李健銘 on 2014/2/8.
+//  Copyright (c) 2014年 TakoBear. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
