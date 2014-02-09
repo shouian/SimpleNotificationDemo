c24aad85dcdc85340448ad5bc566675394ac039d
shouian <amark9@hotmail.com>
2014-02-09 23:33:54 +0800

    - First commit.

---
+++ Notification/TakoButton.m
@@ -0,0 +1,31 @@
+//
+//  TakoButton.m
+//  Notification
+//
+//  Created by 李健銘 on 2014/2/8.
+//  Copyright (c) 2014年 TakoBear. All rights reserved.
+//
+
+#import "TakoButton.h"
+
+@implementation TakoButton
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
